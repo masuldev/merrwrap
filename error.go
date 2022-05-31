5e9f59aaf3cc8f183ab32d0fca788cdec7e2eb00
masuldev <kimmoimm@gmail.com>
2022-05-31 21:46:36 +0900

    git init

---
+++ error.go
@@ -0,0 +1,44 @@
+package merrwarp
+
+type WrapError struct {
+	current error
+	child   error
+}
+
+func (e *WrapError) Current() error {
+	return e.current
+}
+
+func (e *WrapError) Child() error {
+	return e.child
+}
+
+func (e *WrapError) Wrap(err error) *WrapError {
+	return &WrapError{current: err, child: e}
+}
+
+func (e *WrapError) Error() string {
+	if e.current == nil {
+		return ""
+	}
+
+	msg := e.current.Error()
+	if e.child != nil {
+		msg += " " + e.Child().Error()
+	}
+
+	return msg
+}
+
+func Error(err error) *WrapError {
+	if err == nil {
+		return &WrapError{}
+	}
+
+	switch err.(type) {
+	case *WrapError:
+		return err.(*WrapError)
+	default:
+		return &WrapError{current: err}
+	}
+}
---
+++ error_test.go
@@ -0,0 +1,32 @@
+package merrwarp
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestWrapError_Current(t *testing.T) {
+	assert := assert.New(t)
+
+	testErr := fmt.Errorf("[err] test 1")
+
+	emptyErr := Error(nil)
+	existErr := Error(testErr)
+
+	tests := map[string]struct {
+		err    *WrapError
+		output error
+	}{
+		"empty": {err: emptyErr, output: nil},
+		"exist": {err: existErr, output: testErr},
+	}
+
+	fmt.Println(emptyErr)
+	fmt.Println(existErr)
+
+	for _, t := range tests {
+		current := t.err.Current()
+		assert.Equal(t.output, current)
+	}
+}
